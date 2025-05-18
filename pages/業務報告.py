8757d4573b351e380a2d0e16a68ef53881bb0b37
waitback0414 <awesomefactory.kobe@gmail.com>
2025-05-18 12:35:17 +0900

    Update 業務報告.py

--- pages/業務報告.py
+++ pages/業務報告.py
@@ -6,3 +6,6 @@ if st.session_state.get("role") != "user":
 
 st.title("業務報告")
 st.write("ここに通常ユーザー向けの機能を実装します。")
+
+if "authentication_status" in st.session_state and st.session_state["authentication_status"]:
+    st.write(f"ようこそ、{st.session_state['name']}さん！")
