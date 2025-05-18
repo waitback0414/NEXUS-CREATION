bb427b30be3ea8052a7f150e0ed9614e9bda7d88
waitback0414 <awesomefactory.kobe@gmail.com>
2025-05-18 16:44:49 +0900

    Update 業務報告.py

--- pages/業務報告.py
+++ pages/業務報告.py
@@ -54,7 +54,7 @@ filtered_records = [
 ]
 
 st.write(f"いつもご苦労様です、{st.session_state['username']} さん！")
-st.write(f"いつもご苦労様です、{st.session_state['user_email']} さん！")
+# st.write(f"いつもご苦労様です、{st.session_state['user_email']} さん！")
 st.title("業務報告")
 
 if not filtered_records:
